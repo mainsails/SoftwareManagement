76b6f6feb1cb4a350e9245bfe31e8987d655892e
Sam <mainsails@users.noreply.github.com>
2018-12-09 12:44:45 +0000

    Revert "Add ComputerName property to PSSM-QueryUser C# output"
    This reverts commit 68d79e99f4314f5d1ff8eb30e3c8a07af0932694.

--- TypeData/PSSM-QueryUser.cs
+++ TypeData/PSSM-QueryUser.cs
@@ -52,7 +52,6 @@ namespace PSSM
 
         public class TerminalSessionInfo
         {
-            public string ComputerName;
             public string NTAccount;
             public string SID;
             public string UserName;
@@ -271,8 +270,7 @@ namespace PSSM
             try
             {
                 server = OpenServer(ServerName);
-                data.ComputerName = ServerName;
-                
+
                 if (WTSQuerySessionInformation(server, SessionId, WTS_INFO_CLASS.ClientBuildNumber, out buffer, out bytesReturned) == false)
                 {
                     return data;
